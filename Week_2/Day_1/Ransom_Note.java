e62508fe1cf222a5801e2ef696c61e10bc4afb23
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-06-09 20:52:17 +0530

    Added Ransom Note solution

---
+++ Week_2/Day_1/Ransom_Note.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            int index = magazine.indexOf(ch);
+            if (index == -1) {
+                return false;
+            }
+            magazine = magazine.substring(0, index)
+                     + magazine.substring(index + 1);
+        }
+        return true;
+    }
+}
